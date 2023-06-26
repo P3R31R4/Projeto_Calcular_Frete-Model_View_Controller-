f8455dbdfa53939de09045edc4f1902cd2d476db
guilherme.pnunes <guilherme.pnunes@SBC0505246W10-1>
2023-06-26 11:26:25 -0300

    Adicionar arquivos de projeto.

---
+++ Frete/View/FreteForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Frete.View
+{
+    partial class FreteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FreteForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_frete = new System.Windows.Forms.Label();
+            this.cBox_Estado = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_valor = new System.Windows.Forms.TextBox();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(126, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CORREIOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(28, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estado:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(28, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor do Frete:";
+            // 
+            // lb_frete
+            // 
+            this.lb_frete.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lb_frete.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_frete.Location = new System.Drawing.Point(28, 136);
+            this.lb_frete.Name = "lb_frete";
+            this.lb_frete.Size = new System.Drawing.Size(121, 23);
+            this.lb_frete.TabIndex = 3;
+            // 
+            // cBox_Estado
+            // 
+            this.cBox_Estado.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cBox_Estado.FormattingEnabled = true;
+            this.cBox_Estado.Location = new System.Drawing.Point(28, 77);
+            this.cBox_Estado.Name = "cBox_Estado";
+            this.cBox_Estado.Size = new System.Drawing.Size(121, 25);
+            this.cBox_Estado.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(171, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Valor do Produto:";
+            // 
+            // tb_valor
+            // 
+            this.tb_valor.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tb_valor.Location = new System.Drawing.Point(171, 77);
+            this.tb_valor.Name = "tb_valor";
+            this.tb_valor.Size = new System.Drawing.Size(121, 25);
+            this.tb_valor.TabIndex = 6;
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.BackColor = System.Drawing.Color.Gold;
+            this.btn_Calcular.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Calcular.Location = new System.Drawing.Point(171, 121);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(121, 38);
+            this.btn_Calcular.TabIndex = 7;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = false;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // FreteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(326, 194);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.tb_valor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cBox_Estado);
+            this.Controls.Add(this.lb_frete);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FreteForm";
+            this.ShowIcon = false;
+            this.Text = "CORREIOS FRETE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label lb_frete;
+        private ComboBox cBox_Estado;
+        private Label label4;
+        private TextBox tb_valor;
+        private Button btn_Calcular;
+    }
+}
---
+++ Frete/View/FreteForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frete.View
+{
+    public partial class FreteForm : Form
+    {
+        public FreteForm()
+        {
+            InitializeComponent();
+           
+            cBox_Estado.Items.Add("SP");
+            cBox_Estado.Items.Add("RJ");
+            cBox_Estado.Items.Add("BA");
+            cBox_Estado.Items.Add("MG");
+            cBox_Estado.Items.Add("GO");
+            cBox_Estado.Items.Add("PE");
+            cBox_Estado.Items.Add("MT");
+            cBox_Estado.Items.Add("RS");
+        }
+
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            string uf = cBox_Estado.Text;
+            decimal frete = Convert.ToDecimal(tb_valor.Text);
+            decimal valor = 0;
+
+            switch (uf)
+            {
+                case "SP":
+                    valor = frete * 10 / 100;
+                    break;
+            }
+            switch (uf)
+            {
+                case "RJ":
+                    valor = frete * 10 / 100;
+                    break;
+            }
+            switch (uf)
+            {
+                case "BA":
+                    valor = frete * 20 / 100;
+                    break;
+            }
+            switch (uf)
+            {
+                case "MG":
+                    valor = frete * 15 / 100;
+                    break;
+            }
+            switch (uf)
+            {
+                case "GO":
+                    valor = frete * 20 / 100;
+                    break;
+            }
+            switch (uf)
+            {
+                case "PE":
+                    valor = frete *  30 / 100;
+                    break;
+            }
+            switch (uf)
+            {
+                case "MT":
+                    valor = frete * 50 / 100;
+                    break;
+            }
+            switch (uf)
+            {
+                case "RS":
+                    valor = frete * 10 / 100;
+                    break;
+            }
+
+
+            lb_frete.Text = valor.ToString();
+        }
+    }
+    }
